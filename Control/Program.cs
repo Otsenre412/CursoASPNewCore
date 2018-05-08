51668bd5e3d86255834f2f6390432cfd5eb96e65
Ernesto <egn412@hotmail.com>
2018-05-08 11:37:41 +0200

    Antes de irme

---
+++ Control/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Control
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Sentencias de control del lenguaje
+
+            //If else
+
+            Console.WriteLine("Pulsa una tecla...");
+
+            var a = Console.ReadKey();
+
+            var teclaPulsada = a.KeyChar;
+
+            if(teclaPulsada == '5')
+            {
+                Console.WriteLine("Has pulsado 5");
+            }
+            else if(teclaPulsada=='8')
+            {
+                Console.WriteLine("Has pulado el poderoso 8");
+            }
+            else
+            {
+                Console.WriteLine("No tienes ni idea de lo que has pulsado melon!!");
+            }
+
+            // switch
+
+            switch(teclaPulsada)
+            {
+                case 'a':
+                    Console.WriteLine("Añadir elemento");
+                    break;
+                case 'e':
+                    Console.WriteLine("Eliminar elemento");
+                    break;
+                case 'l':
+                    Console.WriteLine("Listar elementos");
+                    break;
+                default:
+                    break;
+            }
+
+            // Iteraciones
+
+            // while y bucle do while
+
+            int i = 0;
+            while(i < 5)
+            {
+                Console.WriteLine(i++);
+            }
+
+            // do while
+
+            int x = 0;
+            do
+            {
+                Console.WriteLine(x++);
+            }
+            while(x < 7);
+
+            // for 
+            Console.WriteLine("Ejecutando bucle for!");
+            for(int b = 0; b < 10; b++)
+            {
+                Console.WriteLine(b);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
