16d867e3524c0366fd83c7123f1b31eaccdb8dae
Ernesto <egn412@hotmail.com>
2018-05-09 14:32:12 +0200

    Visual Estudio

---
+++ MiAPI/MiAPI/Controllers/TodoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MiAPI.Models;
+using MiAPI.Models.Servicios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Todo")]
+    public class TodoController : Controller
+    {
+        FakeTodoServices _service;
+        public TodoController()
+        {
+            _service = new FakeTodoServices();
+        }
+
+        public IList<TodoList> GetLists()
+        {
+            return _service.GetTodoList();
+        }
+    }
+}
---
+++ MiAPI/MiAPI/Models/Servicios/FakeTodoServices.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiAPI.Models.Servicios
+{
+    public class FakeTodoServices
+    {
+        public FakeTodoServices() { }
+
+        public IList<TodoList> GetTodoList()
+        {
+            var lista1 = new TodoList()
+            {
+                Id = 1,
+                Name = "Lita de la compra",
+                Owner = "Ernesto",
+                Items = new List<TodoItem>()
+                {
+                    new TodoItem(){ TodoListId=1, Description= " Huevos"},
+                    new TodoItem(){ TodoListId=1, Description= " Pan"},
+                    new TodoItem(){ TodoListId=1, Description= " Platanos"},
+                    new TodoItem(){ TodoListId=1, Description= " Carne"},
+                }
+            };
+
+            var listCollection = new List<TodoList>
+            {
+                lista1
+            };
+            return listCollection;
+        }
+    }
+}
