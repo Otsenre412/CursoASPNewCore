ee8a16c8b6eca67c9baf5d8713da6d4471a3f56b
Ernesto <egn412@hotmail.com>
2018-05-11 14:19:40 +0200

    ultimo commit

---
+++ APIConsumer/APIConsumer/TodoList.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIConsumer
+{
+    public class TodoList
+    {
+        // Lista
+        public class TodoList
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Owner { get; set; }
+
+            public IList<TodoItem> Items { get; set; }
+        }
+
+        // Elemento de lista
+        public class TodoItem
+        {
+            public int Id { get; set; }
+            public string Description { get; set; }
+            public bool Done { get; set; }
+            public int TodoListId { get; set; }
+
+            [JsonIgnore]
+            public TodoList TodoList { get; set; }
+        }
+    }
+}
